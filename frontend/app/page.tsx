514cbc9a990d603e790d37a3433166fd2492b341
senaunalms <sena.unalmis@fizix.ai>
2026-06-03 16:38:35 +0300

    project bootstrap setup

---
+++ frontend/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black text-white p-8">
+      <h1 className="text-4xl font-bold">
+        CrisisPilot
+      </h1>
+
+      <p className="mt-4 text-gray-400">
+        Global Disruption Intelligence Platform
+      </p>
+    </main>
+  );
+}
